5497a88a8896219e592dcaf94768adcdcca30a67
thibaud75 <81164388+thibaud75@users.noreply.github.com>
2022-06-21 13:32:39 +0200

    switch pc

--- js/cart.js
+++ js/cart.js
@@ -25,7 +25,6 @@ class Cart {
     const arrayCart = [];
     const localStorageCart = JSON.stringify(arrayCart);
     localStorage.setItem("cart", localStorageCart);
-    console.log(arrayCart);
     if (!localStorageCart) {
       console.log("initialisez panier");
     } else if (localStorageCart && localStorageCart.length > 0) {
@@ -38,13 +37,33 @@ class Cart {
     const cart = JSON.parse(localStorage.cart);
     console.log(cart);
     // vérifier si objet présent dans le tableau ou pas
-    const map = cart.map((item) => item.productid);
+    const map = cart.map((item) => ({
+      productid: item.productid,
+      color: item.color,
+    }));
     console.log(map);
 
+    const toto = cart.filter(
+      (cartItem) =>
+        item.productid === cartItem.productid && item.color === cartItem.color
+    );
+    console.log({ toto });
+
+    const index = map.indexOf(item.productid);
     console.log(map.indexOf(item.productid));
+    if (index === -1) {
+      cart.push(item);
+      console.log("Le nouveau tableau est : " + map);
+    } else if (index > -1) {
+      cart[index].quantity = +cart[index].quantity + +item.quantity;
+      console.log(cart[index].quantity);
+      console.log(
+        JSON.stringify(cart[index]) + " existe déjà dans le tableau."
+      );
+    }
+
     // si objet présent et > 0 on incrémente la ligne en question
     // sinon on ajoute l'objet au tab
-    cart.push(item);
     localStorage.cart = JSON.stringify(cart);
     console.log({ cart });
   }
