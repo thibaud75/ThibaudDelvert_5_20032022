5705be99245343c17a4b34c9a615115ef0af5365
Thibaud91 <59475637+Thibaud91@users.noreply.github.com>
2022-06-27 15:26:29 +0200

    ajout du cart dans localstorage

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,3 @@
-// const displayCart = () => {
-//   let canapArticle = document.createElement("article");
-//   document.querySelector("#cart__items").appendChild(canapArticle);
-//   canapArticle.className = "cart__item";
-//   canapArticle.setAttribute("data-id", productid);
-//   canapArticle.setAttribute("data-color", color);
-//   console.log(canapArticle);
-//   let canapDiv = document.createElement("div");
-//   canapDiv.className = "cart__item__img";
-//   canapArticle.appendChild(canapDiv);
-//   console.log(canapDiv);
-//   let canapImg = document.createElement("img");
-//   canapImg.src = canap.imageUrl;
-//   canapDiv.appendChild(canapImg);
-//   console.log(canapImg);
-// };
-
-// displayCart();
 class Cart {
   constructor() {
     this.initCart();
@@ -36,50 +18,61 @@ class Cart {
   addToCart(item) {
     const cart = JSON.parse(localStorage.cart);
     console.log(cart);
-    // vérifier si objet présent dans le tableau ou pas
-
-    // const map = cart.map((item) => ({
-    //   productid: item.productid,
-    //   color: item.color,
-    // }));
-
-    const map = cart.map((item) => item.productid);
-    const map2 = cart.map((item) => item.color);
-    console.log(map);
-    console.log(map2);
-
-    // const toto = cart.filter(
-    //   (cartItem) =>
-    //     item.productid === cartItem.productid && item.color === cartItem.color
-    // );
-    // console.log({ toto });
-
-    const index = map.indexOf(item.productid);
-    const index2 = map2.indexOf(item.color);
-    // console.log(map.indexOf(item.productid));
-    // console.log(map.indexOf(item.color));
-    // const index = map.indexOf(toto);
-    console.log(index);
-    console.log(index2);
-    if (index === -1 || index2 === -1) {
-      cart.push(item);
-      console.log("Le nouveau tableau est : "  );
-    } else if (index > -1 && index2 > -1) {
-        cart[i].quantity = +cart[i].quantity + +item.quantity;
-        console.log(cart[i].quantity);
-        console.log(
-        JSON.stringify(cart[i]) + " existe déjà dans le tableau."
-      );
-      console.log(cart.length);  
-    }
-    // } else if (index2 > -1) {
-    //   cart[index2].quantity = +cart[index2].quantity + +item.quantity;
-    //   console.log(cart[index +1].quantity);
-    // }
-
-    // si objet présent et > 0 on incrémente la ligne en question
-    // sinon on ajoute l'objet au tab
-    localStorage.cart = JSON.stringify(cart);
     console.log({ cart });
+    console.log(item);
+
+    // Création d'une variable foundProduct à laquelle on passe la methode find au tableau cart
+    let foundProduct = cart.find(
+      (p) => p.color === item.color && p.productid === item.productid
+    );
+    console.log(foundProduct);
+    // Foundproduct renvoie undefined si la combinaison de item.color et item.productid
+    // ne sont pas présent dans cart, sinon un objet avec la combinaison de item.color et item.productid
+
+    // Si foundProduct est undefined ça veut dire que la combinaison de item.color et item.productid
+    // n'existe pas dans cart donc on va push l'item dans cart
+    if (foundProduct === undefined) {
+      cart.push(item);
+    } else {
+      // Si foundProduct ne renvoie pas undefined ça veut dire que la combinaison de item.color
+      // et de item.productid est déjà présent donc on va l'incrémenter grâce à foundProduct qui
+      // va nous renvoyer un objet avec la combinaison des deux éléments à laqulle on va ajouter
+      // la quantité qu'on souhaite en lui passant item.quantity
+      foundProduct.quantity = +foundProduct.quantity + +item.quantity;
+      console.log(foundProduct.quantity);
+      console.log(typeof foundProduct);
+      console.log(foundProduct);
+    }
+
+    localStorage.cart = JSON.stringify(cart);
   }
 }
+
+const getCart = () => {
+  const getArrayCart = localStorage.getItem("cart");
+  const parseCart = JSON.parse(getArrayCart);
+  console.log(parseCart);
+};
+
+getCart();
+
+const displayCart = () => {
+  let canapArticle = document.createElement("article");
+  document.querySelector("#cart__items").appendChild(canapArticle);
+  canapArticle.className = "cart__item";
+  canapArticle.setAttribute("data-id", cart[0]);
+  canapArticle.setAttribute("data-color", color);
+  console.log(canapArticle);
+
+  let canapDiv = document.createElement("div");
+  canapDiv.className = "cart__item__img";
+  canapArticle.appendChild(canapDiv);
+  console.log(canapDiv);
+
+  let canapImg = document.createElement("img");
+  canapImg.src = canap.imageUrl;
+  canapDiv.appendChild(canapImg);
+  console.log(canapImg);
+};
+
+displayCart();
